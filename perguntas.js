52b16e03f1129bdfa94df6bfdbeab3d4a3c4ecc0
antonyzl <antony.fernandes.oliveira@escola.pr.gov.br>
2025-10-10 09:25:04 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,35 @@
+criaCartao (
+    'Geografia',
+    'Quem é o maior país do mundo?',
+    'Russia'
+)
 
+criaCartao (
+    'Geografia',
+    'Qual país tem a maior população',
+    'India'
+)
+
+criaCartao (
+    'Matematica',
+    'Quanto é 13+22 ?',
+    '35'
+)
+
+criaCartao (
+    'quizz',
+    'Oque sempre sobe, mas nunca desce ?',
+    'Idade'
+)
+
+criaCartao (
+    'futebol',
+    'Qual time atual de Neymar.jr',
+    'Santos F.C'
+)
+
+criaCartao (
+    'Futebol',
+    'Onde foi a copa do mundo 2022 ?',
+    'Qatar'
+)
